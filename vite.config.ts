0e5e86666f1b36bc4cb90ab88e31fd3fc0ef6ed3
unknown <xabhista19@gmail.com>
2024-08-30 23:15:32 +0700

    feat: completed project

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath, URL } from "url";
 
+const baseURL = `/xi-august-project`;
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: baseURL,
   plugins: [react()],
   resolve: {
     alias: {
