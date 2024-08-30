b6dda889e878e364e125ae4702b7eafba22a619e
unknown <xabhista19@gmail.com>
2024-08-30 23:24:17 +0700

    feat: completed project

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,9 +1,15 @@
+import NotFound from "@/pages/404";
 import Home from "@/pages/home";
 import { createBrowserRouter } from "react-router-dom";
 
+const BaseURL = "/xi-august-project"
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "*",
+    element: NotFound(),
+  },
+  {
+    path: `${BaseURL}`,
     element: Home(),
   },
 ]);
